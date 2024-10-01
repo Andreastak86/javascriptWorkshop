fe7e35b7ae2b42944301a78cdd092fb6ce3d7257
Andreas <andreas.takvam@gmail.com>
2024-10-01 18:39:45 +0200

    done

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 //JavaScript tutorial
-document.getElementById("generateList").addEventListener("click", function () {
-    const list = document.getElementById("list");
-    list.innerHTML = ""; // Tømmer listen hvis den allerede har elementer
-
-    const items = ["Eple", "Banan", "Appelsin", "Druer", "Jordbær"];
-
-    for (let i = 0; i < items.length; i++) {
-        const listItem = document.createElement("li");
-        listItem.textContent = items[i];
-        list.appendChild(listItem);
-        console.log(items[i]);
-    }
+document.querySelector("#mainBtn").addEventListener("click", function () {
+    alert("Knappen ble klikket!");
 });
