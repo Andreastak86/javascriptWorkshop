25b52327ad0d79f769ab87f2ff8ed70ba4fca718
Andreas <andreas.takvam@gmail.com>
2024-10-01 18:13:31 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//JavaScript tutorial
+
+let myName = "Andreas"; //En variabel med tekst -  string
+const age = 37; //En variabel med tall - numbers
+const isDeveloper = true; //En variabel som endten er sann eller usann - boolean
+
+//Vi kan bruke javascript til å gjøre matematiske operasjoner
+let result = 5 + 3;
+console.log(result); // ved hjelp av console.log(), kan man se resultatet i nettleseren
+
+//eller til å sette sammen en tekst med en variabel
+let greeting = "hello " + myName;
+console.log(greeting);
+
+//Funksjoner
+
+function great() {
+    //her lager vi funksjonen. Vi må bruke () og {} for å beskrive hva vi ønsker
+    console.log("Hello World!"); //her printer vi ut "Hello World!" som vi kan se i nettleseren
+}
+great(); //her kjører vi funksjonen
